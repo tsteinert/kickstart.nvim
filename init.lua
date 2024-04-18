24fe82ece7e0509054196cb9ff09cf553a9ed7c2
Torsten Steinert <steinert@brak.de>
2024-04-18 13:00:59 +0200

    added java formater

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        java = { 'google-java-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
