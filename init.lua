9372997cc95d3e940e6ef55ed85550b0840b9edd
Torsten Steinert <torstensteinert@mailbox.org>
2024-04-21 21:44:40 +0200

    added html-lsp

--- init.lua
+++ init.lua
@@ -540,6 +540,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        html = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
