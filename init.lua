abfe14189f57b1d57a57698aa2018e58f13ac59e
Torsten Steinert <torstensteinert@web.de>
2024-10-13 22:55:39 +0200

    change conform-config - notify_on_error -> true, increased timeout

--- init.lua
+++ init.lua
@@ -642,14 +642,14 @@ require('lazy').setup({
       },
     },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 5000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
